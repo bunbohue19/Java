1cb810bcca965c7b94a2f422e75c34f5e2ee5d0c
Lộc Sẹo <95389934+bunbohue19@users.noreply.github.com>
2022-03-22 10:11:31 +0700

    Add files via upload

---
+++ JavaFxEmailClientCourse/src/com/barosanu/controller/LoginWindowController.java
@@ -0,0 +1,24 @@
+package com.barosanu.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginWindowController {
+
+    @FXML
+    private TextField emailAddressField;
+
+    @FXML
+    private Button errorLabel;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    void loginButtonAction() {
+    	System.out.println("click!!");
+    }
+
+}
